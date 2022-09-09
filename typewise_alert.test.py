01523c703969209270cfe79773ddfd35277aae18
nidhiv98 <72518009+nidhiv98@users.noreply.github.com>
2022-09-09 14:51:33 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,10 +1,9 @@
 import unittest
 import typewise_alert
 
-
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW'
+    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
   def test_infers_breach_as_per_limit(self):
     self.assertTrue(typewise_alert.infer_breach(112, 50, 100) == 'TOO_HIGH')
     
