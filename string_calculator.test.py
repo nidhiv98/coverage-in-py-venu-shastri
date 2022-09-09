06bbe4de457f54280f1d3cdbba8009119e8e1ea5
nidhiv98 <72518009+nidhiv98@users.noreply.github.com>
2022-09-09 15:50:10 +0530

    Update string_calculator.test.py

--- string_calculator.test.py
+++ string_calculator.test.py
@@ -1,9 +1,13 @@
 import unittest
+#import string_calculator
 
+
+def add(num):
+  
 
 class String_calculatorTest(unittest.TestCase):
   def given_empty_string_zero_expected(self):
-    self.assertTrue(string_calculator.add("0") == 0)
+    self.assertTrue(add("0") == 0)
   
 
 if __name__ == '__main__':
